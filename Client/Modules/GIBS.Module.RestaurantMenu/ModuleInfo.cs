fb446f83dc31a12897972c34577e432c6b01dc78
JoeAucoin <jaucoin@gibs.com>
2026-03-30 13:23:12 -0400

    new release: 1.0.12

--- Client/Modules/GIBS.Module.RestaurantMenu/ModuleInfo.cs
+++ Client/Modules/GIBS.Module.RestaurantMenu/ModuleInfo.cs
@@ -11,9 +11,9 @@ namespace GIBS.Module.RestaurantMenu
         {
             Name = "RestaurantMenu",
             Description = "Restaurant Menu",
-            Version = "1.0.11",
+            Version = "1.0.12",
             ServerManagerType = "GIBS.Module.RestaurantMenu.Manager.RestaurantMenuManager, GIBS.Module.RestaurantMenu.Server.Oqtane",
-            ReleaseVersions = "1.0.0,1.0.4,1.0.8,1.0.9,1.0.10,1.0.11",
+            ReleaseVersions = "1.0.0,1.0.4,1.0.8,1.0.9,1.0.10,1.0.11,1.0.12",
             Dependencies = "GIBS.Module.RestaurantMenu.Shared.Oqtane",
             PackageName = "GIBS.Module.RestaurantMenu" ,
             Owner = "Global Internet Business Solutions LLC",
